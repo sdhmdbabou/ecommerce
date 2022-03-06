3c4eb22eb0186f279eb4fa68427ee72e7590c74c
sdbabou <sdbabou@gmail.com>
2022-03-06 11:21:56 +0100

    crate nav and shearch co

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountComponent } from './component/account/account.component';
+import { CardComponent } from './component/card/card.component';
+import { CategoryComponent } from './component/category/category.component';
+import { HomeComponent } from './component/home/home.component';
+import { ShopComponent } from './component/shop/shop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'category',component:CategoryComponent},
+  {path:'card',component: CardComponent},
+  {path:'shop',component:ShopComponent},
+  {path:'account',component:AccountComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { CategoryComponent } from './component/category/category.component';
+import { CardComponent } from './component/card/card.component';
+import { ShopComponent } from './component/shop/shop.component';
+import { AccountComponent } from './component/account/account.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { SearchComponent } from './component/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CategoryComponent,
+    CardComponent,
+    ShopComponent,
+    AccountComponent,
+    NavbarComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
