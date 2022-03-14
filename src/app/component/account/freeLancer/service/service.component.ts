f780745f5d7be8f943a4a56ffac3c51fc049cb49
sdbabou <sdbabou@gmail.com>
2022-03-14 14:23:47 +0100

    service componet wvate

--- src/app/component/account/freeLancer/service/service.component.ts
+++ src/app/component/account/freeLancer/service/service.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./service.component.css']
 })
 export class ServiceComponent implements OnInit {
-
-  constructor() { }
+   saveUsername=false;
+ 
+  constructor() { 
+    
+  }
 
   ngOnInit() {
   }
 
+
+
+  public onSaveUsernameChanged(value:boolean){
+      this.saveUsername = value;
+    console.log( value)}
+   
 }
